9e188a022da8502652245b33afbf28eef790de49
yderek5 <yderek5@gmail.com>
2017-12-11 19:26:29 -0600

    fix table

--- bamazonManager.js
+++ bamazonManager.js
@@ -61,7 +61,7 @@ function printTable() {
       // For each item in the array log the info
       table.push(
         [item.item_id, item.product_name, item.department_name,
-          item.price, item.stock_quantity
+        + "$" +  item.price + ".00", item.stock_quantity
         ]
       );
     });
